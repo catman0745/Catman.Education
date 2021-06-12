8fafd62ca120c5377e8415c8fc5cfaa737fc33f7
catman0745 <catman0745@gmail.com>
2021-06-12 16:17:54 +0300

    Fix user querying

--- Catman.Education.Application/Features/User/Queries/GetUsers/GetUsersQueryHandler.cs
+++ Catman.Education.Application/Features/User/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -1,6 +1,7 @@
 namespace Catman.Education.Application.Features.User.Queries.GetUsers
 {
     using System.Linq;
+    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
     using Catman.Education.Application.Extensions;
     using Catman.Education.Application.Abstractions;
@@ -11,14 +12,18 @@ namespace Catman.Education.Application.Features.User.Queries.GetUsers
 
     internal class GetUsersQueryHandler : ResourceRequestHandlerBase<GetUsersQuery, Paginated<User>>
     {
+        private static IQueryable<User> Users(IApplicationStore store, string role) =>
+            role switch
+            {
+                nameof(Admin) => store.Admins,
+                nameof(Student) => store.Students,
+                object unmatched => throw new SwitchExpressionException(unmatched)
+            };
+
         private static IQueryable<User> UsersFilter(IQueryable<User> users, GetUsersQuery getQuery)
         {
             var username = getQuery.Username?.ToUpper() ?? string.Empty;
-            var role = getQuery.Role?.ToUpper() ?? string.Empty;
-
-            return users
-                .Where(user => user.Username.ToUpper().Contains(username))
-                .Where(user => user.Role.ToUpper().Contains(role));
+            return users.Where(user => user.Username.ToUpper().Contains(username));
         }
         
         private readonly IApplicationStore _store;
@@ -32,7 +37,7 @@ namespace Catman.Education.Application.Features.User.Queries.GetUsers
         
         protected override async Task<ResourceRequestResult<Paginated<User>>> HandleAsync(GetUsersQuery getQuery)
         {
-            var users = await _store.Users
+            var users = await Users(_store, getQuery.Role)
                 .ApplyFilter(UsersFilter, getQuery)
                 .OrderBy(user => user.Username)
                 .PaginateAsync(getQuery);
